8569d7b2833c63d45f61f3aef92230983ca41dc6
Atari <76502035+Akari-Glitch@users.noreply.github.com>
2022-02-23 00:23:05 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongodb:{
-		URI: 'mongodb+srv://fernando:puntomarket@puntomarket-cluster.5qwpk.mongodb.net/puntomarket-database?retryWrites=true&w=majority'
+		URI: 'URI'
 	}
 }
